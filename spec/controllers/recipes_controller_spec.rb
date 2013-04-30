8dacc9eec6b656339ca362f4b34994fe45a58173
mzp <mzpppp@gmail.com>
2013-04-30 04:59:05 +0400

    add validation for Recipe

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -23,7 +23,7 @@ describe RecipesController do
   # This should return the minimal set of attributes required to create a valid
   # Recipe. As you add validations to Recipe, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "name" => "MyString" } }
+  let(:valid_attributes) { { "name" => "MyString", "tasks" => '-' } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Recipe do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_params) do
+    { name: 'host', tasks: '-' }
+  end
+
+  describe 'validates' do
+    it '(if this test fail, CHECK `valid_params`' do
+      Recipe.create(valid_params).should_not be_validation_error
+    end
+
+    describe '.name' do
+      it 'should not contain blank' do
+        Recipe.create(valid_params.merge(name: '')).should be_validation_error
+      end
+
+      it 'should not contain nil' do
+        Recipe.create(valid_params.merge(name: nil)).should be_validation_error
+      end
+    end
+
+    describe '.tasks' do
+      it 'should not contain blank' do
+        Recipe.create(valid_params.merge(tasks: '')).should be_validation_error
+      end
+
+      it 'should not contain nil' do
+        Recipe.create(valid_params.merge(tasks: nil)).should be_validation_error
+      end
+    end
+  end
 end
