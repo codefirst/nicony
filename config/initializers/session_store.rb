7c27c68a7bd478f75e07c7bc9e52ad6d5f4effed
mallowlabs <mallowlabs@gmail.com>
2013-05-02 20:30:09 +0900

    Updated: to Rails4 RC1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Nicony::Application.config.session_store :encrypted_cookie_store, key: '_nicony_session'
+Nicony::Application.config.session_store :cookie_store, key: '_nicony_session'
