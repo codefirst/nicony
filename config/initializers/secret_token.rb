cb3b35837da9b4286df868ace38221807ed00bb2
mallowlabs <mallowlabs@gmail.com>
2013-06-17 20:12:48 +0900

    Updated: Rails4 RC2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
 # Make sure the secret is at least 30 characters and all random,
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Nicony::Application.config.secret_key_base = '68f3a1b48c589706e65f87443fc8a04c530e59c4206e0ea3e6ebd9822f67e585e7625507fea1af35feee1322518a55852e08c250ddf363bcfcbd65d3def1eea0'
+Nicony::Application.config.secret_key_base = '832707201d66c9da49482868d08e32655c06e72f442d970afb06dcb4e92ed6a4e11e94e3455ead70e849f8b4a193150382cd30b8dd58bcf2f3630e7f2cf95a0b'
